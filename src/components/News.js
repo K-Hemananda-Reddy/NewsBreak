e6a109b5b2d1bbe9ea0f72f3df71114ab6481c0d
Asaduddin aman <83344388+K-Hemananda-Reddy@users.noreply.github.com>
2023-01-13 18:19:39 +0530

    Initialized NewsBreak app

---
+++ src/components/News.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewsItem from './NewsItem'
+
+export default function News() {
+  return (
+    <div>This is a news component
+        <NewsItem/>
+    </div>
+  )
+}
---
+++ src/components/NewsItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewsItem() {
+  return (
+    <div>This is a news item</div>
+  )
+}
