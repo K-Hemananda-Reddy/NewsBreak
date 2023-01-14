b37639868b26b37a3e1ab81ae41007016d6345d5
Asaduddin aman <83344388+K-Hemananda-Reddy@users.noreply.github.com>
2023-01-14 11:53:11 +0530

    added loading spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,17 @@
 import React, { useEffect } from 'react'
 import NewsItem from './NewsItem'
 import { useState } from 'react'
+import Spinner from './Spinner';
+import '../App.css';
 
 export default function News() {
   const [articles,setArticles]=useState([]);
   const [page,setPage]=useState(1);
+  const [loading,setLoading]=useState(false);
 
   const fetchData=async()=>{
     let url="https://newsapi.org/v2/everything?q=football&apiKey=4e6edafdf0f34830af7a2e61db34f667&page=1&pageSize=18"
+    setLoading(true);
     let data= await fetch(url);
 
     let pd=data.json();
@@ -15,6 +19,7 @@ export default function News() {
    pd.then((result)=>{
     setArticles(result.articles);
    })
+   setLoading(false);
   };
 
   useEffect(()=>{
@@ -23,6 +28,7 @@ export default function News() {
 
   const handlePrevClick=async()=>{
     let url=`https://newsapi.org/v2/everything?q=football&apiKey=4e6edafdf0f34830af7a2e61db34f667&page=${page-1}&pageSize=18`
+    setLoading(true);
     let data= await fetch(url);
 
     let pd=data.json();
@@ -30,9 +36,11 @@ export default function News() {
    pd.then((result)=>{
     setArticles(result.articles);
    })
+   setLoading(false);
   }
   const handleNextClick=async()=>{
     let url=`https://newsapi.org/v2/everything?q=football&apiKey=4e6edafdf0f34830af7a2e61db34f667&page=${page+1}&pageSize=18`
+    setLoading(true);
     let data= await fetch(url);
 
     let pd=data.json();
@@ -40,16 +48,21 @@ export default function News() {
    pd.then((result)=>{
     setArticles(result.articles);
    })
+   setLoading(false);
   }
 
 
 
   return (
     <div className='container my-3'>
-      <h1>NewsBreak - Top Headlines</h1>
+      <h1 className='text-center'>NewsBreak - Top Headlines</h1>
+      {loading && <div className="text-center container">
+      <Spinner/>
+      </div>}
+      
         <div className="row">
 
-          {articles.map((element)=>{
+          {!loading && articles.map((element)=>{
             return <div className="col-md-4" key={element.url}>
             <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage?element.urlToImage:"https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/11787/production/_124395517_bbcbreakingnewsgraphic.jpg"} newsUrl={element.url}/>
           </div>
---
+++ src/components/Spinner.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../App.css'
+export default function Spinner() {
+  return (
+    <div className="lds-facebook "><div></div><div></div><div></div></div>
+  )
+}
