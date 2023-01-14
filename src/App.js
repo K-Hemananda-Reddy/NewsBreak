e165dd2051009b38ece061814c3f66056f727777
Asaduddin aman <83344388+K-Hemananda-Reddy@users.noreply.github.com>
2023-01-14 15:00:53 +0530

    added categories

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar/>
-      <News country="in" category="sports"/>
-    </>
+      <Routes>
+        <Route exact path='/' element={<News key="general" country="in" category="general"/>}/>
+        <Route exact path='/business' element={<News key="business" country="in" category="business"/>}/>
+        <Route exact path='/entertainment' element={<News key="entertainment" country="in" category="entertainment"/>}/>
+        <Route exact path='/general' element={<News key="general" country="in" category="general"/>}/>
+        <Route exact path='/science' element={<News key="science" country="in" category="science"/>}/>
+        <Route exact path='/health' element={<News key="health" country="in" category="health"/>}/>
+        <Route exact path='/sports' element={<News key="sports" country="in" category="sports"/>}/>
+        <Route exact path='/technology' element={<News key="technology" country="in" category="technology"/>}/>
+
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
